a3f215018301d84558fb340bd1a57037ee01e7f0
Saggitarie <saggitarie@gmail.com>
2020-05-17 18:46:54 +0900

    Hardcode host name for graphql for the endpoint

--- server/schema.js
+++ server/schema.js
@@ -10,7 +10,7 @@ const SERVER = new ApolloServer({
   typeDefs: TYPEDEFS,
   resolvers: RESOLVERS,
   playground: {
-    endpoint: `${process.env.HEROKU_HOST}:${PORT}/graphql`,
+    endpoint: `https://want2drink.herokuapp.com/:${PORT}/graphql`,
     // endpoint: `${process.env.REACT_APP_LOCAL_HOST}:${process.env.PORT}/graphql`,
     settings: {
       'editor.theme': 'light'
