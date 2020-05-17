416c6cf321b7741adf32163eec63093dc566a145
Saggitarie <saggitarie@gmail.com>
2020-05-17 14:23:51 +0900

    Redeploy with configs set on heroku for maps and 0-auth

--- server/index.js
+++ server/index.js
@@ -11,9 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
-  APP.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html'));
 }
 
 // Middleware: GraphQL
