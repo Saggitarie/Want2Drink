dc4d85073bc11afaa983216bc615a6ace162274e
Saggitarie <saggitarie@gmail.com>
2020-05-17 17:04:24 +0900

    Mocked Polly's code

--- server/index.js
+++ server/index.js
@@ -4,27 +4,30 @@ const express = require('express');
 const {SERVER} = require('./schema.js');
 
 const APP = express();
+const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  APP.use(express.static('build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  APP.get('*', (req, res) => {
-    res.sendFile('/build/index.html');
-  });
+	APP.use(express.static('client/build'));
 }
+APP.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+});
 
 // Middleware: GraphQL
   SERVER.applyMiddleware({
+  path: "/graphql"
   app: APP
 });
 // Express: Port
 const PORT = process.env.PORT || 4000;
 
 // Express: Listener
-APP.listen(PORT, () => {
-  console.log(`The server has started on port: ${PORT}`);
-  console.log(`${process.env.REACT_APP_HEROKU_HOST}:${PORT}/graphql`);
-});
+// APP.listen(PORT, () => {
+//   console.log(`The server has started on port: ${PORT}`);
+//   console.log(`${process.env.REACT_APP_HEROKU_HOST}:${PORT}/graphql`);
+// });
+
+APP
+.listen({ port: process.env.PORT || 4000 }, ()=> {
+  console.log(`🚀 app running`)
+})
